9d4eb24f8b27aec80cbf21484d85f38a0e96a386
Anurag Nagar <nagaranurag1999@gmail.com>
2023-09-22 08:15:30 +0530

    Format: improve code formating and add docstring

--- flask_blog/static/js/app/addpost.js
+++ flask_blog/static/js/app/addpost.js
@@ -1,8 +1,10 @@
 "use strict";
 
+// Define constant for post `title` and `sub-title` element.
 const postTitle = document.getElementById("title");
 const postSubTitle = document.getElementById("subtitle");
 
+// Define constant for post `title` and `sub-title` length element.
 const titleLength = document.getElementById("title-length");
 const subTitleLength = document.getElementById("subtitle-length");
 
@@ -11,8 +13,14 @@ const submitButton = document.getElementById("submit");
 const titleTextLimit = 100;
 const subtitleTextLimit = 150;
 
+/**
+ * Event listener for the post title input field.
+ * Displays the character count and hides/shows the character count element.
+ */
 postTitle.addEventListener("input", () => {
-  titleLength.textContent = `${postTitle.value.length} / ${titleTextLimit}`;
+  titleLength.textContent = `${postTitle.value.length}`;
+
+  // If the post title is empty, hide the character count element; otherwise, show it.
   if (postTitle.value.length == 0) {
     titleLength.setAttribute("hidden", "");
   } else {
@@ -20,8 +28,14 @@ postTitle.addEventListener("input", () => {
   }
 });
 
+/**
+ * Event listener for the post sub-title input field.
+ * Displays the character count and hides/shows the character count element.
+ */
 postSubTitle.addEventListener("input", () => {
-  subTitleLength.textContent = `${postSubTitle.value.length} / ${subtitleTextLimit}`;
+  subTitleLength.textContent = `${postSubTitle.value.length}`;
+
+  // If the post title is empty, hide the character count element; otherwise, show it.
   if (postSubTitle.value.length == 0) {
     subTitleLength.setAttribute("hidden", "");
   } else {
@@ -29,15 +43,37 @@ postSubTitle.addEventListener("input", () => {
   }
 });
 
+/**
+ * Set the source of the target image element 
+ * to the loaded image data.
+ **/
+const setCoverImage = (e, image) => {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("show-image").removeAttribute("hidden");
+  image.src = e.target.result;
+}
+
+/**
+ * Display a preview of the selected cover image 
+ * with loading animation.
+ **/
 const coverImagePreview = (input, element) => {
+
+  let loadingTime = 2500;
   let selectedFile = input.files[0];
   let fileReader = new FileReader();
   let image = document.querySelector(element);
 
   image.title = selectedFile.name;
+
+  // the FileReader finishes loading the image after setTimeout .
   fileReader.onload = (input) => {
-    document.getElementById("show-cover-image").removeAttribute("hidden");
-    image.src = input.target.result;
+    document.getElementById("show-image").setAttribute("hidden", "");
+    document.getElementById("loader").style.display = "flex";
+    // Set a timeout to replace the image source with the loaded image after the loadingTime.
+    setTimeout(() => setCoverImage(input, image), loadingTime);
   };
+
+  // Read the selected image file as a data URL.
   fileReader.readAsDataURL(selectedFile);
 };
--- flask_blog/static/js/app/editpost.js
+++ flask_blog/static/js/app/editpost.js
@@ -1,27 +1,80 @@
 "use strict";
 
 const postTitle = document.getElementById("title");
-const postSubtitle = document.getElementById("subtitle");
+const postSubTitle = document.getElementById("subtitle");
+
 const titleLength = document.getElementById("title-length");
-const subtitleLength = document.getElementById("subtitle-length");
+const subTitleLength = document.getElementById("subtitle-length");
 
 const submitBtn = document.getElementById("submit");
+
 const titleTextLimit = 100;
 const subtitleTextLimit = 150;
 
-titleLength.textContent = postTitle.value.length + "/" + titleTextLimit;
-subtitleLength.textContent =
-  postSubtitle.value.length + "/" + subtitleTextLimit;
+titleLength.textContent = postTitle.value.length;
+subTitleLength.textContent = postSubTitle.value.length;
 
-const coverImageChange = (input) => {
+/**
+ * Event listener for the post title input field.
+ * Displays the character count and hides/shows the character count element.
+ */
+postTitle.addEventListener("input", () => {
+  titleLength.textContent = `${postTitle.value.length}`;
+
+  // If the post title is empty, hide the character count element; otherwise, show it.
+  if (postTitle.value.length === 0) {
+    titleLength.setAttribute("hidden", "");
+  } else {
+    titleLength.removeAttribute("hidden");
+  }
+});
+
+/**
+ * Event listener for the post sub-title input field.
+ * Displays the character count and hides/shows the character count element.
+ */
+postSubTitle.addEventListener("input", () => {
+  subTitleLength.textContent = `${postSubTitle.value.length}`;
+
+  // If the post title is empty, hide the character count element; otherwise, show it.
+  if (postSubTitle.value.length === 0) {
+    subTitleLength.setAttribute("hidden", "");
+  } else {
+    subTitleLength.removeAttribute("hidden");
+  }
+});
+
+/**
+ * Set the source of the target image element 
+ * to the loaded image data.
+ **/
+const setCoverImage = (e, image) => {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("show-image").removeAttribute("hidden");
+  image.src = e.target.result;
+}
+
+
+/**
+ * Display a preview of the selected cover image 
+ * with loading animation.
+ **/
+const coverImageChange = (input, element) => {
+
+  let loadingTime = 2500;
   let selectedFile = input.files[0];
   let fileReader = new FileReader();
-  let coverImage = document.querySelector('#cover-image-show');
+  let coverImage = document.querySelector(element);
 
   coverImage.title = selectedFile.name;
+
   fileReader.onload = (input) => {
-    document.getElementById("show-cover-image").removeAttribute("hidden");
-    coverImage.src = input.target.result;
+    document.getElementById("show-image").setAttribute("hidden", "");
+    document.getElementById("loader").style.display = "flex";
+    // Set a timeout to replace the image source with the loaded image after the loadingTime.
+    setTimeout(() => setCoverImage(input, coverImage), loadingTime);
   };
+
+  // Read the selected image file as a data URL.
   fileReader.readAsDataURL(selectedFile);
 };
