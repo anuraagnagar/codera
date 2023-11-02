0e396ca4f8e631699acb2f0256199980a7f51700
Anurag Nagar <nagaranurag1999@gmail.com>
2023-11-02 20:29:30 +0530

    Updated

--- flask_blog/__version__.py
+++ flask_blog/__version__.py
@@ -3,8 +3,8 @@ VERSION = (1, 0, 1)
 
 __version__ = ".".join(map(str, VERSION))
 __status__ = "Available"
-__title__ = "CodeCircle"
+__title__ = "Codera"
 __description__ = "A Blog Application powered by Flask"
 __author__ = "Anurag Nagar"
 __email__ = "nagaranurag1999@gmail.com"
-__license__ = ""
+__license__ = "BSD-3-Clause"
