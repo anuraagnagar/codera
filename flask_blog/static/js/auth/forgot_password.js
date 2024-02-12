09853aa5b79370a144c523ecda96819b611d04a0
Anurag Nagar <nagaranurag1999@gmail.com>
2024-02-12 12:59:54 +0530

    rename: forget.js to forgot_password.js

---
+++ flask_blog/static/js/auth/forgot_password.js
@@ -0,0 +1,40 @@
+"use-strict";
+
+const emailInput = document.getElementById("email");
+const submitBtn = document.getElementById("submit");
+const emailError = document.getElementById("error");
+
+const emailRegEx =
+  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+(function () {
+  if (emailInput.value.match(emailRegEx)) {
+    submitBtn.removeAttribute("disabled");
+  }
+})();
+
+/**
+ * Validates an email address and updates the UI accordingly.
+ * If the email is invalid, adds error styling, displays an error message,
+ * and disables the submit button.
+ */
+const emailValidator = () => {
+  if (emailInput.value.length > 0) {
+    if (emailInput.value.match(emailRegEx)) {
+      emailInput.classList.remove("error");
+      emailError.setAttribute("hidden", "true");
+      emailError.textContent = "";
+      submitBtn.removeAttribute("disabled");
+    } else {
+      emailInput.classList.add("error");
+      emailError.removeAttribute("hidden");
+      emailError.textContent = "Invalid email address.";
+      submitBtn.setAttribute("disabled", "true");
+    }
+  } else {
+    emailInput.classList.remove("error");
+    emailError.textContent = "";
+  }
+};
+
+emailInput.addEventListener("input", emailValidator);
